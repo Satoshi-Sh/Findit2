04f05e46ed0e9332d74c037237163d96552c44e1
satoshi-sh <satoss1108@gmail.com>
2023-05-16 11:24:52 -0500

    Add design to movie detail page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
 const Header = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className="sticky-top">
       <Container>
         <Navbar.Brand>
           <span className="blue">F</span>
--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -2,9 +2,37 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./moviedetail.css";
+import data from "../data/genre_ids.json";
+import Button from "react-bootstrap/Button";
 
-const MovieDetail = () => {
+interface GenreIds {
+  [id: number]: string;
+}
+const genreIds: GenreIds = data;
+
+const imageBaseURL = "https://image.tmdb.org/t/p/w342";
+
+interface MovieData {
+  poster_path: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+  id: number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+}
+
+const MovieDetail: React.FC = () => {
   const { title } = useParams();
+  const [data, setData] = useState<MovieData | null>();
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
     const URL = process.env.REACT_APP_THIRD_API_URL;
     const ApiKey = process.env.REACT_APP_THIRD_API_KEY;
@@ -18,13 +46,52 @@ const MovieDetail = () => {
           `${URL}/search/movie?api_key=${ApiKey}&query=${urlTitle}`
         );
         console.log(response.data.results[0]);
+        setData(response.data.results[0]);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching users:", error);
       }
     };
     fetchMovie();
-  });
-  return <h1>{title}</h1>;
+  }, []);
+  if (loading) {
+    return (
+      <div className="movie-detail">
+        <h1>Loading....</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="movie-detail">
+      {data ? (
+        <>
+          <h1 className="movie-title">{data.title}</h1>
+          <img src={`${imageBaseURL}/${data.poster_path}`} alt="movie poster" />
+          <div className="genres">
+            Genre:
+            {data.genre_ids &&
+              data.genre_ids.map((genre, i) => {
+                return <div key={i}>{genreIds[genre]}</div>;
+              })}
+          </div>
+          <div className="overview">
+            <p>{data.overview}</p>
+          </div>
+          <Button className="add-watch" variant="outline-info">
+            Watch List +
+          </Button>{" "}
+          <footer>
+            Data Provided by{" "}
+            <a className="tmdb-link" href="https://www.themoviedb.org/">
+              TMDB
+            </a>
+          </footer>
+        </>
+      ) : (
+        <h1>No data available..</h1>
+      )}
+    </div>
+  );
 };
 
 export default MovieDetail;
