9f373712aecbb386a4d481778f6471142a8cec4c
satoshi-sh <satoss1108@gmail.com>
2023-05-15 19:19:13 -0500

    Setup .env and make a http request to the 3rd party api to get data

--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -5,6 +5,25 @@ import "./moviedetail.css";
 
 const MovieDetail = () => {
   const { title } = useParams();
+  useEffect(() => {
+    const URL = process.env.REACT_APP_THIRD_API_URL;
+    const ApiKey = process.env.REACT_APP_THIRD_API_KEY;
+    let urlTitle = "";
+    if (typeof title !== "undefined") {
+      urlTitle = title.replace("%20", "+");
+    }
+    const fetchMovie = async () => {
+      try {
+        const response = await axios.get(
+          `${URL}/search/movie?api_key=${ApiKey}&query=${urlTitle}`
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+    fetchMovie();
+  });
   return <h1>{title}</h1>;
 };
 
