fd4f5baeb31bf5522dfa2cf88bd7ec2eefe03527
satoshi-sh <satoss1108@gmail.com>
2023-05-17 16:55:55 -0500

    changed page_url in const.tsx.

--- src/constant.tsx
+++ src/constant.tsx
@@ -1,2 +1,2 @@
 export const BASE_URL = "https://weak-puce-kingfisher-toga.cyclic.app";
-export const PAGE_URL = "http://localhost:3000/findit2";
+export const PAGE_URL = "https://satoshi-sh.github.io/findit2";
